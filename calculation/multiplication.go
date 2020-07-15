7e21b8c6a065659896a0ecd675ad981d56523737
Abimanyu Gatya Dhaneswara <manyuabim9@gmail.com>
2020-07-16 03:15:50 +0700

    Chall Multiplication

--- calculation/multiplication.go
+++ calculation/multiplication.go
@@ -1,9 +1,10 @@
 package calculation
 
 func Multiplication(number int, numberSecond int) int {
-	return multiplication(number, numberSecond)
-}
-
-func multiplication(number int, numberSecond int) int {
+	// return multiplication(number, numberSecond)
 	return number * numberSecond
 }
+
+// func multiplication(number int, numberSecond int) int {
+// 	return number * numberSecond
+// }
